0f8104741994432647a6a3fbdcc4f385b646fdef
rokib-tkc <rokib.uddin@tkc.co.uk>
2026-07-13 20:49:17 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbw6Oj74AtUHcnSAdu81IBeVar0oh0MOkCTchZFdqB4hbGw4NhJXKOkN81C-Z5NMl33I/exec",
-  APP_VERSION: "1.3.7",
-  SYNC_INTERVAL_MS: 25000
-};
-
