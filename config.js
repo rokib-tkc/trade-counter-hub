07c10c0ad44395153688b230ed2c666c1c40afcd
rokib-tkc <rokib.uddin@tkc.co.uk>
2026-07-15 20:38:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbw6Oj74AtUHcnSAdu81IBeVar0oh0MOkCTchZFdqB4hbGw4NhJXKOkN81C-Z5NMl33I/exec",
-  APP_VERSION: "2.0.0",
+  APP_VERSION: "2.0.1",
   SYNC_INTERVAL_MS: 25000
 };
 
